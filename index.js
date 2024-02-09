f788d7f3244973f72f5a1068907eb765bf03b017
stephanygrace <stephanytayong73@gmail.com>
2024-02-09 22:30:15 +0800

    add Read function for findOne and find

--- index.js
+++ index.js
@@ -10,36 +10,73 @@ async function main() {
 
     try {
         await client.connect();
-        await createManyListing(client, [
-            {
-            name: "Stephany Grace",
-            summary: "Chill Vibes",
-            bedrooms: 1,
-            bathrooms: 1,
-            },
-            {
-                name: "Grace",
-                summary: "Chill",
-                bedrooms: 2,
-                bathrooms: 2,
-            },
-            {
-                name: "Stephany",
-                summary: "Vibes",
-                bedrooms: 3,
-                bathrooms: 3,
-            },
-        ] )
+        // await createManyListing(client, [
+        //     {
+        //     name: "Stephany Grace",
+        //     summary: "Chill Vibes",
+        //     bedrooms: 1,
+        //     bathrooms: 1,
+        //     },
+        //     {
+        //         name: "Grace",
+        //         summary: "Chill",
+        //         bedrooms: 2,
+        //         bathrooms: 2,
+        //     },
+        //     {
+        //         name: "Stephany",
+        //         summary: "Vibes",
+        //         bedrooms: 3,
+        //         bathrooms: 3,
+        //     },
+        // ] )
+
+        // await findOneListingByName(client, "Stephany")
+
+        await findListingsWithFilter(client, {
+            minimumNumberOfBedrooms: 4,
+            minimumNumberOfBathrooms: 2,
+            maximumNumberOfResults: 5
+        })
     }
     catch (e) {
         console.log(e)
     } finally {
         await client.close();
     }
-
 }
 
 main().catch(console.error);
+
+async function findListingsWithFilter(client, {
+    minimumNumberOfBedrooms = 0,
+    minimumNumberOfBathrooms = 0,
+    maximumNumberOfResults = Number.MAX_SAFE_INTEGER
+} = {}) {
+    const cursor = await client.db("sample_airbnb").collection("listingsAndReviews").find({
+        bedrooms: { $gte: minimumNumberOfBedrooms},
+        bathrooms: { $gte: minimumNumberOfBathrooms}
+    }).sort({ last_review: -1})
+    .limit(maximumNumberOfResults);
+
+    const result = await cursor.toArray();
+    if(result){
+        console.log(result)
+    } else {
+        console.log("No Listings Found")
+    }
+}
+
+async function findOneListingByName(client, nameOfListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({name: nameOfListing});
+
+    if (result){
+        console.log(result)
+    } else {
+        console.log("No document found!")
+    }
+}
+
 
 async function createListing(client, newListing) {
     const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
